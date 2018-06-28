d89032324065f656da03efc1742f2dbe37540362
JZJisawesome <40674968+JZJisawesome@users.noreply.github.com>
2018-06-28 17:01:58 -0400

    make helloworld.cpp better
    also add documentation

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <cstdint>
 
+constexpr std::uint8_t changes {2};///<Number of changes to this file
+
+/** \brief main function for helloworld.cpp
+ *
+ * \author JZJisawesome
+ * \date 2018
+*/
 int main()
 {
-  std::cout << "Hello world";
+  std::cout << "Hello World! Changes so far of this file: " << static_cast<int> (changes);
+  std::cout << std::endl;
   
   return 0;
 }
